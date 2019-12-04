24096de6fc8cb6cb157a384f0af8a9b2878c21fe
Dylan Sweeney <dylanjamessweeney@gmail.com>
2019-12-03 20:48:03 -0500

    please work

---
+++ navbar.js
@@ -0,0 +1,36 @@
+document.write('<div class="navbar"><button class="btn" style="pointer-events: none; float:left cursor: default;color: rgb(260, 25, 160 );">SARA</button>\
+<div class="dropdown">\
+    <button class="dropbtn" id="search_tag">\
+    &#x1F50D Search <span style="font-size: 10px">&#9660</span><i class="fa fa-caret-down"></i> </button>\
+    <div class="dropdown-content">\
+    <a class="active" href="index.html" id="search_fixed">Fixed</a>\
+    <a class="active" href="index.html" id="search_upload">Upload</a>\
+    <a class="active" href="index.html" id="search_api">API</a>\
+    <a class="active" href="index.html" id="search_db">Database</a>\
+    </div>\
+  </div>\
+  <div class="dropdown">\
+    <button class="dropbtn">&#x1f4d6 Course <span style="font-size: 10px">&#9660</span><i class="fa fa-caret-down"></i></button>\
+    <div class="dropdown-content">\
+      <a  id="courses_button" href="https://learn.zybooks.com/zybook/CUNYCSCI355TeitelmanSpring2019">Zybooks</a>\
+    </div>\
+  </div>\
+  <div class="dropdown">\
+  <button class="dropbtn">&#x1F310 Browser <span style="font-size: 10px">&#9660</span><i class="fa fa-caret-down"></i></button>\
+    <div class="dropdown-content">\
+      <a href="#" id="nav">Navigator</a>\
+      <a href="#" id="wind">Window</a>\
+      <a href="#" id="screen">Screen</a>\
+      <a href="#" id="location">Location</a>\
+      <a href="#" id="geolocation">Geolocation</a>\
+    </div>\
+  </div>\
+  <div class="dropdown" >\
+    <button class="dropbtn" id="about_tag"  >&#x2754 About <span style="font-size: 10px">&#9660</span><i class="fa fa-caret-down"></i></button>\
+    <div class="dropdown-content">\
+    <a href="#" id="about">About</a>\
+    </div>\
+  </div>\
+  </div>');
+
+
---
+++ search_screen.js
@@ -0,0 +1,14 @@
+document.write('<div class="google">\
+<a href="#" id="sara_logo"><img src="sara.png" defer> </img></a>\
+</div>\
+<!-- FORM SEARCH -->\
+<div class="form">\
+<form>\
+  <label for="form-search"> </label>\
+  <input type="text" id="form-search" value="new wave bands" readonly><br>\
+</form>\
+</div>\
+<!-- BUTTONS -->\
+<div class="buttons">\
+    <input id="search_button" type="button" value=" Sara Search" id="google_search">\
+</div>');
